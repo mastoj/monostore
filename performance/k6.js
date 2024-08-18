e85134d0360d77f4f6a4d7e305ade1696f054c02
Tomas Jansson <tomas.jansson@gmail.com>
2024-08-19 00:09:57 +0200

    More k6 tests

--- performance/k6.js
+++ performance/k6.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 export const options = {
-  vus: 2000,
+  vus: 100,
   duration: '20s',
 };
 
@@ -8,7 +8,29 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const getRandomUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+const randomUUIDS = Array.from({length: 1000}, () => getRandomUUID());
+
+const createdCarts = {};
+
 export default function () {
-  const id = getRandomInt(1, 1000);
+  const id = randomUUIDS[getRandomInt(0, randomUUIDS.length - 1)];
+  if(!createdCarts[id]) {
+    http.post('http://localhost:5170/cart', JSON.stringify({
+      cartId: id,
+    }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    createdCarts[id] = true
+  }
   http.get('http://localhost:5170/cart/' + id);
 }
