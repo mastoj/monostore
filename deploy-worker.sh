5f4dde56546c0eb6daa4e6fe87577a5b32cb6cf4
Tomas Jansson <tomas.jansson@gmail.com>
2024-11-11 12:27:23 +0100

    Force new tag

--- deploy-worker.sh
+++ deploy-worker.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo "Deploying API"
+echo "Deploying $APP"
 LABEL=$(git rev-parse --short HEAD)
-echo "Deploying image $APP:$LABEL to resource group $AZ_RESOURCE_GROUP and environment $AZ_CONTAINER_APP_ENV"
+echo "Deploying image $DOCKER_SERVER/$APP:$LABEL to resource group $AZ_RESOURCE_GROUP and environment $AZ_CONTAINER_APP_ENV"
 
-az containerapp create \
-  --name $APP \
-  --resource-group $AZ_RESOURCE_GROUP \
-  --environment $AZ_CONTAINER_APP_ENV \
-  --image $DOCKER_SERVER/$APP:$LABEL \
-  --ingress external \
-  --registry-server $DOCKER_SERVER \
+# az containerapp create \
+#   --name $APP \
+#   --resource-group $AZ_RESOURCE_GROUP \
+#   --environment $AZ_CONTAINER_APP_ENV \
+#   --image $DOCKER_SERVER/$APP:$LABEL \
+#   --ingress external \
+#   --registry-server $DOCKER_SERVER \
   # --query properties.configuration.ingress.fqdn
 
   # Update the container ap with a new image
 az containerapp update \
   --name $APP \
   --resource-group $AZ_RESOURCE_GROUP \
-  --image $DOCKER_SERVER/$APP:$LABEL
+  --image $DOCKER_SERVER/$APP:$LABEL \
+  --debug
